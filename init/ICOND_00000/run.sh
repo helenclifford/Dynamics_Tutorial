3779a594de10210182e70a1d06bd07e0def035b8
Helen Clifford <helenclifford@uchicago.edu>
2026-07-27 22:21:53 -0500

    Add files via upload

---
+++ init/ICOND_00000/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# $-N init_00000
+
+
+
+PRIMARY_DIR=/project/lgagliardi/helenclifford/practice_rcc/sharc4/methaniminium/tutorial_reproduction/init/ICOND_00000//
+
+cd $PRIMARY_DIR
+
+
+$SHARC/SHARC_MOLCAS.py QM.in > QM.log 2> QM.err
---
+++ init/all_run_init.sh
@@ -0,0 +1,48 @@
+#/bin/bash
+
+CWD=/project/lgagliardi/helenclifford/practice_rcc/sharc4/methaniminium/tutorial_reproduction/init
+
+cd $CWD/ICOND_00000//
+bash run.sh
+cd $CWD
+echo ICOND_00000/ >> DONE
+cd $CWD/ICOND_00001//
+bash run.sh
+cd $CWD
+echo ICOND_00001/ >> DONE
+cd $CWD/ICOND_00002//
+bash run.sh
+cd $CWD
+echo ICOND_00002/ >> DONE
+cd $CWD/ICOND_00003//
+bash run.sh
+cd $CWD
+echo ICOND_00003/ >> DONE
+cd $CWD/ICOND_00004//
+bash run.sh
+cd $CWD
+echo ICOND_00004/ >> DONE
+cd $CWD/ICOND_00005//
+bash run.sh
+cd $CWD
+echo ICOND_00005/ >> DONE
+cd $CWD/ICOND_00006//
+bash run.sh
+cd $CWD
+echo ICOND_00006/ >> DONE
+cd $CWD/ICOND_00007//
+bash run.sh
+cd $CWD
+echo ICOND_00007/ >> DONE
+cd $CWD/ICOND_00008//
+bash run.sh
+cd $CWD
+echo ICOND_00008/ >> DONE
+cd $CWD/ICOND_00009//
+bash run.sh
+cd $CWD
+echo ICOND_00009/ >> DONE
+cd $CWD/ICOND_00010//
+bash run.sh
+cd $CWD
+echo ICOND_00010/ >> DONE
