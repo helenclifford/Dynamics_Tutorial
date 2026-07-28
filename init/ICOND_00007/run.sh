296e55ebe83b90569c624ea7341766aa81b42bb8
Helen Clifford <helenclifford@uchicago.edu>
2026-07-27 22:23:32 -0500

    Add files via upload

---
+++ init/ICOND_00007/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# $-N init_00007
+
+
+
+PRIMARY_DIR=/project/lgagliardi/helenclifford/practice_rcc/sharc4/methaniminium/tutorial_reproduction/init/ICOND_00007//
+
+cd $PRIMARY_DIR
+
+if [ -d ../ICOND_00000/SAVE ];
+then
+  if [ -d ./SAVE ];
+  then
+    rm -r ./SAVE
+  fi
+  cp -r ../ICOND_00000/SAVE ./
+else
+  echo "Should do a reference overlap calculation, but the reference data in ../ICOND_00000/ seems not OK."
+  exit 1
+fi
+
+
+$SHARC/SHARC_MOLCAS.py QM.in > QM.log 2> QM.err
